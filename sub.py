f96aef45318d0a1cb2e3540bd0b56f3c48763542
parita pooj <pari.pooj@gmail.comgit config --global github.user paritagit config --global user.email :pari.pooj@gmail.com>
2011-09-03 19:23:23 +0530

    renames subtitle files with respective .avi file's name

--- sub.py
+++ sub.py
@@ -4,23 +4,39 @@ path=raw_input("PATH:")
 listing = os.listdir(path)
 sublist=os.listdir(path+"\\subtitles\\")
 for filename in listing:
-    i=filename[:-4]
-    count=0
-    for filename in sublist:
-        if('.db' in filename):
-            []
-        else:
-            pcount=0
-            j=filename[:-4]
-            max=j
-            for a in range(0,len(i)):
-                if((x for x in j if x.isalpha())==(y for y in i if y.isalpha())):
-                    print x, y
-                    pcount=pcount+1
-            if(pcount>count):
-                count=pcount
-                max=j
-    oldname=path+"\\subtitles\\"+max+".jpg"
-    newname=path+"\\subtitles\\"+i+'.srt'
-    print oldname, newname
-    os.rename(oldname,newname)
+    if not('.avi' in filename):
+        []
+    else:
+        a=filename[:-4]
+        count=0
+        i=''
+        for x in a:
+            if x.isalpha():
+                i=i+x
+        print i
+        maxc=''
+        for filename in sublist:
+            if not('.srt' in filename):
+                []
+            else:
+                pcount=0
+                b=filename[:-4]
+                j=''
+                for y in b:
+                    if y.isalpha():
+                        j=j+y
+                print j
+                for z in range(0,min(len(i),len(j))):
+                    if(i[z]==j[z]):
+                        pcount=pcount+1
+                        print pcount
+                if(pcount>count):
+                    count=pcount
+                    maxc=b
+                    print maxc
+        oldname=path+"\\subtitles\\"+maxc+".srt"
+        newname=path+"\\subtitles\\"+a+'.srt'
+        print oldname, newname
+        os.rename(oldname,newname)
+
+    
