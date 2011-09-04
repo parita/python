251fa5dde2a39c44d549c683af04b327606a4c61
parita pooj <pari.pooj@gmail.comgit config --global github.user paritagit config --global user.email :pari.pooj@gmail.com>
2011-09-04 15:12:44 +0530

    renames subtitles files with the respective .avi file's name

--- sub.py
+++ sub.py
@@ -3,36 +3,25 @@ import os
 path=raw_input("PATH:")
 listing = os.listdir(path)
 sublist=os.listdir(path+"\\subtitles\\")
+d={}
+for filename in sublist:
+    if not('.srt' in filename):
+        pass
+    else:
+        j=''.join(x for x in filename if x.isalpha())
+        j=j[:-5]
+        d[j]=filename
+        print j
+
 for filename in listing:
     if not('.avi' in filename):
-        []
+        pass
     else:
         a=filename[:-4]
-        count=0
-        i=''
-        for x in a:
-            if x.isalpha():
-                i=i+x
-        maxc=''
-        for filename in sublist:
-            if not('.srt' in filename):
-                []
-            else:
-                pcount=0
-                b=filename[:-4]
-                j=''
-                for y in b:
-                    if y.isalpha():
-                        j=j+y
-                for z in range(0,min(len(i),len(j))):
-                    if(i[z]==j[z]):
-                        pcount=pcount+1
-                if(pcount>count):
-                    count=pcount
-                    maxc=b
-        oldname=path+"\\subtitles\\"+maxc+".srt"
-        newname=path+"\\subtitles\\"+a+'.srt'
+        i=''.join(x for x in a if x.isalpha())
+        print i
+        max=''.join(y for y in d.keys() if ((y in i) or (i in y)))
+        oldname=path+"\\subtitles\\"+d[max]
+        newname=path+"\\subtitles\\"+a+'.en.srt'
         print oldname, newname
         os.rename(oldname,newname)
-
-    
