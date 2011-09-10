5aaf1e161cbb4936ad44b01c09ebe7cc7efb1ec7
parita pooj <pari.pooj@gmail.comgit config --global github.user paritagit config --global user.email :pari.pooj@gmail.com>
2011-09-10 23:55:30 +0530

    multiple client chat

--- socket-programming/echo-client.py
+++ socket-programming/echo-client.py
@@ -2,7 +2,7 @@ import socket
 import sys 
 
 host = 'localhost' 
-port = 5000 
+port = 8000 
 size = 1024 
 client= socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 client.connect((host,port))
@@ -10,17 +10,19 @@ print client.getsockname()
 print '%'
 data=''
 while 1:
-	# read from keyboard
-    	if data:
-                print 'Received:',data,'%'   
-        else:
-                data = raw_input("Data:")
+        # read from keyboard
+        if not data:
+                data=raw_input("Data:")
                 if data:
                         client.send(data)
                         print 'Sent:',data,'%'
                         data=''
                 else:
-                        data=client.recv(size)
-                 
-    	
+                        while not data:
+                                data=client.recv(size)
+                                print 'Received:',data,'%'
+                        data=''
+        else:
+                print 'Received:',data,'%' 
+
 client.close()
--- socket-programming/echo-server.py
+++ socket-programming/echo-server.py
@@ -1,52 +1,40 @@
 # TCP server example
-import socket
-import select
-
+import socket           
+import select           
+ 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("", 5000))
-server_socket.listen(5)
-
-print "TCPServer Waiting for client on port 5000"
-
-data ='initialised...'
+server_socket.bind(("", 8000))                                          
+server_socket.listen(5)                                                 
+ 
+print "TCPServer Waiting for client on port 5000"                       
+ 
+data =''               
 input_ = [server_socket]
-output = [server_socket]
+output = []       
 
+ 
 while 1:
-        inputready,outputready,exceptready = select.select(input_,output,[])
-        print len(inputready) , len(input_), len(outputready), len(output)
-
-        for s in inputready:
-                if s==server_socket:
-                        client, address = server_socket.accept()
-                        print "I got a connection from ", address
-                        input_.append(client)
+        inputready,outputready,exceptready = select.select(input_,output,[])    
+        print len(inputready) , len(input_), len(outputready), len(output) 
             
-                else:
-                        if data:
-                            s.send(data)
-                        data=s.recv(1024)
-                        print "received:",data
-                        if data:
-                                print "out of inputready"
+        for s in inputready:                   
+                if s==server_socket:            
+                        client, address = server_socket.accept()        
+                        print "I got a connection from ", address       
+                        input_.append(client)                           
+                        output.append(client)                           
+                else:                                                   
+                        data=s.recv(1024)    
+                        if data:                                        
+                                print "received:",data
                                 break;
                         else:
                                 input_.remove(s)
-        inputready,outputready,exceptready = select.select(input_,output,[])
-        print len(inputready) , len(input_), len(outputready), len(output)
-        for s in outputready:
-                if s==server_socket:
-                        client, address = server_socket.accept()
-                        print "I got a connection from ", address
-                        output.append()
-                else:
-                        if data:
-                                s.send(data)
-                                output.remove(s)
-                        else:
-                                s.close()
-                inputready,outputready,exceptready = select.select(input_,output,[])
-                print len(inputready) , len(input_), len(outputready), len(output)
-        data=''
-   
-    
+                
+        inputready,outputready,exceptready = select.select(input_,output,[])    
+        print len(inputready) , len(input_), len(outputready), len(output) 
+        for s in outputready:                                           
+                if data:                                                
+                        s.send(data)                                    
+                        output.remove(s)                               
+                                                  
