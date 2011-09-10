6920e324e900de53eba3166605c6651952e0703d
parita pooj <pari.pooj@gmail.comgit config --global github.user paritagit config --global user.email :pari.pooj@gmail.com>
2011-09-10 16:34:38 +0530

    for client socket

--- socket-programming/echo-client.py
+++ socket-programming/echo-client.py
@@ -2,19 +2,24 @@ import socket
 import sys 
 
 host = 'localhost' 
-port = 50000 
+port = 5000 
 size = 1024 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.connect((host,port)) 
-sys.stdout.write('%') 
+client= socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+client.connect((host,port))
+ 	
+print '%'
 
-while 1: 
-    # read from keyboard 
-    line = sys.stdin.readline() 
-    if line == ' ': 
-        break 
-    s.send(line) 
-    data = s.recv(size) 
-    sys.stdout.write(data) 
-    sys.stdout.write('%') 
-s.close()
+while 1:
+	print client.getsockname()
+    	# read from keyboard
+    	data = raw_input("Data:")
+	print 'Sent:',data,'%'
+	data = client.recv(size)
+    	
+	if data == '': 
+        	break 
+    	client.send(data) 
+    	data = client.recv(size)
+    	print 'Received:',data
+    	print '%' 
+client.close()
