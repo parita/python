b869c182ade148c621cc79a9d5fea573b96d590e
parita pooj <pari.pooj@gmail.comgit config --global github.user paritagit config --global user.email :pari.pooj@gmail.com>
2011-09-13 21:17:58 +0530

    for server socket

--- socket-programming/echo-server.py
+++ socket-programming/echo-server.py
@@ -10,28 +10,29 @@ print "TCPServer Waiting for client on port 8000"
  
 data ='init'           
 input_ = [server_socket]
-output = []       
+output = []
+name={}
 
 inputready,outputready,exceptready = select.select(input_,output,[])    
-
-while 1:
-                    
+print len(inputready),len(outputready),len(input_),len(output)
+while 1:            
         for s in inputready:                   
                 if s==server_socket:            
                         client, address = server_socket.accept()        
-                        print "I got a connection from ", address       
+                        print "I got a connection from ", address
+                        nick=client.recv(1024)
+                        name[client]=nick
+                        print 'name:',nick
                         input_.append(client)                           
                         output.append(client)                           
                 else:
                         k=s.recv(1024)
                         if k:
-                                data=data+'\n'+k
-                        if data:                                        
-                                print "received:",data
-                inputready,outputready,exceptready = select.select(input_,output,[])    
-                
-                                   
-        inputready,outputready,exceptready = select.select(input_,output,[])    
+                                data=data+'\n'+name[s]+':'+k
+                                print data
+                    
+        inputready,outputready,exceptready = select.select(input_,output,[])
+
         for s in outputready:
                 s.send(data)
         data=''
